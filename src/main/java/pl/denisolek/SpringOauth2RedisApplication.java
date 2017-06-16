366e40bf79f7824030fcf4d3e1835dc0c2d97fb4
Denis Olek <denis.olek@gmail.com>
2017-06-16 22:40:40 +0200

    init

---
+++ src/main/java/pl/denisolek/SpringOauth2RedisApplication.java
@@ -0,0 +1,12 @@
+package pl.denisolek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2RedisApplication.class, args);
+	}
+}
